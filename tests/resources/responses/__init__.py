1bf2eef8e81de08bd07a07e24fd6a650cf34dbce
taschenb <taschenb@posteo.de>
2019-04-08 23:27:05 +0200

    test: response: Add artist/get
    Add the example response from the API-documentation for getting
    further information of an album.
    
    Request:
    http://www.qobuz.com/api.json/0.2/
    artist/get
    ?app_id=100000000
    &artist_id=36819

--- tests/resources/responses/__init__.py
+++ tests/resources/responses/__init__.py
@@ -6,5 +6,8 @@ with open("tests/resources/responses/album_get.json") as json_data:
 with open("tests/resources/responses/album_search.json") as json_data:
     album_search_json = json.load(json_data)
 
+with open("tests/resources/responses/artist_get_albums.json") as json_data:
+    artist_get_albums_json = json.load(json_data)
+
 with open("tests/resources/responses/artist_search.json") as json_data:
     artist_search_json = json.load(json_data)
