e2e1173cfab36407a01084098ee8b0f3247000ca
taschenb <taschenb@posteo.de>
2019-04-08 22:24:42 +0200

    test: artist: Test searching
    Test the searching for an artist

---
+++ tests/artist/test_artist_search.py
@@ -0,0 +1,72 @@
+import pytest
+import qobuz
+import responses
+
+from tests.resources.responses import artist_search_json
+
+
+qobuz.api.APP_ID = "request_from_api@qobuz.com"
+
+
+def get_url(query, limit=50, offset=0):
+    return (
+        qobuz.api.API_URL
+        + "artist/search"
+        + "?query={0}".format(query)
+        + "&limit={}".format(limit)
+        + "&offset={}".format(offset)
+        + "&app_id={}".format(qobuz.api.APP_ID)
+    )
+
+
+@pytest.fixture
+def response_search_empty():
+    with responses.RequestsMock() as response_mock:
+        url = get_url("")
+        resp = {
+            "query": "",
+            "artists": {"limit": 50, "offset": 0, "total": 0, "items": []},
+        }
+
+        response_mock.add(
+            responses.GET, url, json=resp, status=200, match_querystring=True
+        )
+
+        yield response_mock
+
+
+@pytest.fixture
+def response_search():
+    with responses.RequestsMock() as response_mock:
+        response_mock.add(
+            responses.GET,
+            url=get_url(artist_search_json["query"]),
+            json=artist_search_json,
+            status=200,
+            match_querystring=True,
+        )
+
+        yield response_mock
+
+
+def test_search_len(response_search):
+    artists = qobuz.Artist.search(artist_search_json["query"])
+
+    assert len(artists) != 0
+    assert len(artists) == len(artist_search_json)
+
+
+def test_search_found(response_search):
+    artists = qobuz.Artist.search(artist_search_json["query"])
+
+    assert artists[0].name == "MGMT"
+    assert artists[0].picture is None
+    assert artists[0].id == 118680
+    assert artists[0].albums_count == 29
+    assert artists[0].slug == "mgmt"
+
+
+def test_search_empty(response_search_empty):
+    artists = qobuz.Artist.search("")
+
+    assert len(artists) == 0
