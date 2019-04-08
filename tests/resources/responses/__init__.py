a711383d53088d846f369d8d45d077c4a1bc860d
taschenb <taschenb@posteo.de>
2019-04-08 20:38:46 +0200

    test: response: Add artist/search
    Add the example response from the API-documentation for searching for
    an artist.
    
    Request:
    http://www.qobuz.com/api.json/0.2/
    artist/search
    ?app_id=100000000
    &query=MGMT
    &limit=2

--- tests/resources/responses/__init__.py
+++ tests/resources/responses/__init__.py
@@ -2,3 +2,6 @@ import json
 
 with open("tests/resources/responses/album_get.json") as json_data:
     album_get_json = json.load(json_data)
+
+with open("tests/resources/responses/artist_search.json") as json_data:
+    artist_search_json = json.load(json_data)
