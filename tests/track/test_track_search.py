d82abc83e07201b2746deea42cb9db6c0864e83d
taschenb <taschenb@posteo.de>
2019-04-09 00:10:01 +0200

    test: track: Test searching
    Test the searching for a track

---
+++ tests/track/test_track_search.py
@@ -0,0 +1,53 @@
+import pytest
+import qobuz
+import responses
+
+from tests.resources.responses import track_search_json
+
+
+qobuz.api.APP_ID = "request_from_api@qobuz.com"
+
+
+def get_url(query, limit=50, offset=0):
+    return (
+        qobuz.api.API_URL
+        + "track/search"
+        + "?query={}".format(query)
+        + "&limit={}".format(limit)
+        + "&offset={}".format(offset)
+        + "&app_id={}".format(qobuz.api.APP_ID)
+    )
+
+
+@pytest.fixture
+def response_track_search():
+    with responses.RequestsMock() as response_mock:
+        response_mock.add(
+            responses.GET,
+            get_url(query=track_search_json["query"]),
+            json=track_search_json,
+            status=200,
+            match_querystring=True,
+        )
+        yield response_mock
+
+
+def test_track_search_len(response_track_search):
+    tracks = qobuz.Track.search(track_search_json["query"])
+
+    assert len(tracks) == len(track_search_json["tracks"]["items"])
+
+
+def test_track_search_type(response_track_search):
+    tracks = qobuz.Track.search(track_search_json["query"])
+
+    for t in tracks:
+        assert isinstance(t, qobuz.Track)
+
+
+def test_track_search_content(response_track_search):
+    tracks = qobuz.Track.search(track_search_json["query"])
+
+    track_items = track_search_json["tracks"]["items"]
+    assert tracks[0] == qobuz.Track(track_items[0])
+    assert tracks[1] == qobuz.Track(track_items[1])
