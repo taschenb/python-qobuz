27a9765c6be76a746a70359cb75a69a290e6bca4
taschenb <taschenb@posteo.de>
2019-04-09 00:44:43 +0200

    test: artist: Test Artist from ID
    Test the initialization of an Artist from the lookup of an ID

--- tests/artist/test_artist_init.py
+++ tests/artist/test_artist_init.py
@@ -1,7 +1,16 @@
 import qobuz
+import responses
 
 from tests.resources.responses import artist_search_json
-from tests.resources.fixtures import artist
+
+
+def get_url(artist_id):
+    return (
+        qobuz.api.API_URL
+        + "artist/get"
+        + "?artist_id={}".format(artist_id)
+        + "&app_id={}".format(qobuz.api.APP_ID)
+    )
 
 
 def test_artist_init():
@@ -14,3 +23,20 @@ def test_artist_init():
     assert artist.picture == artist_item["picture"]
     assert artist.slug == artist_item["slug"]
     assert artist.albums_count == artist_item["albums_count"]
+
+
+def test_artist_from_id():
+    artist_item = artist_search_json["artists"]["items"][0]
+
+    with responses.RequestsMock() as response_mock:
+        response_mock.add(
+            responses.GET,
+            url=get_url(artist_item["id"]),
+            json=artist_item,
+            status=200,
+            match_querystring=True,
+        )
+
+        artist_from_id = qobuz.Artist.from_id(artist_item["id"])
+
+    assert artist_from_id == qobuz.Artist(artist_item)
