adb376e3ee3bd94048f74a564cad068af476b206
taschenb <taschenb@posteo.de>
2019-04-08 23:29:00 +0200

    feat: artist: Get all albums
    Get all albums from an artist

--- src/qobuz/artist.py
+++ src/qobuz/artist.py
@@ -31,6 +31,33 @@ class Artist(object):
             and self.albums_count == other.albums_count
         )
 
+    def get_all_albums(self, offset=0, limit=50):
+        """Return albums of an artist.
+
+        Parameters
+        ----------
+        limit: int
+            Number of elements returned per request
+        offset: int
+            Offset from which to obtain limit elements
+
+        Returns
+        -------
+        list of Album
+            Albums from the artist
+        """
+        from qobuz import Album
+
+        albums = api.request(
+            "artist/get",
+            artist_id=self.id,
+            extra="albums",
+            offset=offset,
+            limit=limit,
+        )
+
+        return [Album(a) for a in albums["albums"]["items"]]
+
     @classmethod
     def search(cls, artist, limit=50, offset=0):
         """Search for an artist.
