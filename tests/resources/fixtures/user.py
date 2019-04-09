5344772e769fa1b06c7f5b90d6fd38b1c3d5f2b8
taschenb <taschenb@posteo.de>
2019-04-09 09:49:50 +0200

    test: fixture: Add user
    Create a User fixture to be used without always having to mock the
    login everywhere

---
+++ tests/resources/fixtures/user.py
@@ -0,0 +1,24 @@
+import json
+import pytest
+import qobuz
+import responses
+
+from tests.user.test_user_login import get_url
+from tests.resources.responses import user_login_json
+
+
+@pytest.fixture
+def user():
+    username = "username"
+    password = "password"
+
+    with responses.RequestsMock() as response_mock:
+        response_mock.add(
+            responses.GET,
+            url=get_url(username, password),
+            json=user_login_json,
+            status=200,
+            match_querystring=False,
+        )
+
+        return qobuz.User(username, password)
