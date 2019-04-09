57c04e29a1f18379d4132f88d0f7681dba9b45a2
taschenb <taschenb@posteo.de>
2019-04-09 14:51:55 +0200

    test: playlist: Test searching for playlists
    Search for playlists

---
+++ tests/playlist/test_playlist_search.py
@@ -0,0 +1,49 @@
+import pytest
+import qobuz
+import responses
+
+from tests.resources.responses import playlist_search_json
+
+
+qobuz.api.APP_ID = "request_from_api@qobuz.com"
+
+
+def get_url(query, limit=50, offset=0):
+    return (
+        qobuz.api.API_URL
+        + "playlist/search"
+        + "?query={0}".format(query)
+        + "&limit={}".format(limit)
+        + "&offset={}".format(offset)
+        + "&app_id={}".format(qobuz.api.APP_ID)
+    )
+
+
+@pytest.fixture
+def response_search():
+    with responses.RequestsMock() as response_mock:
+        response_mock.add(
+            responses.GET,
+            url=get_url(playlist_search_json["query"]),
+            json=playlist_search_json,
+            status=200,
+            match_querystring=True,
+        )
+
+        yield response_mock
+
+
+def test_search_len(response_search):
+    playlists = qobuz.Playlist.search(playlist_search_json["query"])
+
+    assert len(playlists) != 0
+    assert len(playlists) == len(playlist_search_json["playlists"]["items"])
+
+
+def test_search_content(response_search):
+    playlists = qobuz.Playlist.search(playlist_search_json["query"])
+    playlists_resp = playlist_search_json["playlists"]["items"]
+
+    assert playlists[0].id == playlists_resp[0]["id"]
+    assert playlists[0].name == playlists_resp[0]["name"]
+    assert playlists[0].description == playlists_resp[0]["description"]
