5e7676cadc5e3448067e43ceff0a7a7e1144bce9
taschenb <taschenb@posteo.de>
2019-04-09 15:16:20 +0200

    test: user: Delete existing playlist
    Test deleting a existing playlist of the user

---
+++ tests/user/test_user_playlist_delete.py
@@ -0,0 +1,32 @@
+import qobuz
+import responses
+
+from tests.resources.fixtures import user, playlist
+from tests.resources.responses import playlist_get_tracks_json
+from tests.resources.responses import playlist_add_tracks_json
+
+
+qobuz.api.APP_ID = "request_from_api@qobuz.com"
+
+
+def get_url(playlist_id, user_auth_token):
+    return (
+        qobuz.api.API_URL
+        + "playlist/delete"
+        + "?playlist_id={}".format(playlist_id)
+        + "&user_auth_token={}".format(user_auth_token)
+        + "&app_id={}".format(qobuz.api.APP_ID)
+    )
+
+
+def test_playlist_delete(playlist, user):
+    with responses.RequestsMock() as response_mock:
+        response_mock.add(
+            responses.GET,
+            get_url(playlist_id=playlist.id, user_auth_token=user.auth_token),
+            json={"status": "success"},
+            status=200,
+            match_querystring=True,
+        )
+
+        assert user.playlist_delete(playlist)
