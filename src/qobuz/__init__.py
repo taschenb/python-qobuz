7fd4022805a6c44280e2be197120e02aafe09c04
taschenb <taschenb@posteo.de>
2019-04-09 09:40:00 +0200

    feat: user: Add User with login
    In order to be able to perform certain requests, a valid
    user_auth_token has to be provided. That token is received by logging
    in.

--- src/qobuz/__init__.py
+++ src/qobuz/__init__.py
@@ -1,5 +1,6 @@
 from .artist import Artist
 from .album import Album
 from .track import Track
+from .user import User
 
-__all__ = ["Artist", "Album", "Track"]
+__all__ = ["Artist", "Album", "Track", "User"]
---
+++ src/qobuz/user.py
@@ -0,0 +1,57 @@
+import hashlib
+
+class User(object):
+    """Own user to be logged in.
+
+    Some operations require an authenticated user.
+
+    Parameters
+    ----------
+    username: str
+        Username or e-mail of the user
+    password: str
+        Password for the username
+    """
+
+    def __init__(self, username, password):
+        self.username = username
+        self.auth_token = self.login(self.username, password)
+
+    def login(self, username, password):
+        """Login and return a authentication token.
+
+        Parameters
+        ----------
+        username: str
+            Username or e-mail of the user
+        password: str
+            Password for the username
+
+        Returns
+        -------
+        str
+            User's authentication token
+        """
+        user = api.request(
+            "user/login",
+            username=username,
+            password=self._hash_password(password),
+        )
+
+        return user["user_auth_token"]
+
+    @staticmethod
+    def _hash_password(password):
+        """Hash the password with MD5.
+
+        Parameters
+        ----------
+        password: str
+            Plain password
+
+        Returns
+        -------
+        str
+            Hashed password to be used for logging in
+        """
+        return hashlib.md5(password.encode()).hexdigest()
