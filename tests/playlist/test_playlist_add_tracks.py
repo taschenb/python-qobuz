60aefa6884f1222cfe1216c4804f4e5e9bace959
taschenb <taschenb@posteo.de>
2019-04-09 15:06:41 +0200

    test: playlist: Add tracks to a playlist
    Test adding tracks to a existing playlist

---
+++ tests/playlist/test_playlist_add_tracks.py
@@ -0,0 +1,48 @@
+import qobuz
+import responses
+
+from tests.resources.fixtures import user, playlist
+from tests.resources.responses import playlist_get_tracks_json
+from tests.resources.responses import playlist_add_tracks_json
+
+
+qobuz.api.APP_ID = "request_from_api@qobuz.com"
+
+
+def get_url(playlist_id, user_auth_token, track_ids=""):
+    return (
+        qobuz.api.API_URL
+        + "playlist/addTracks"
+        + "?playlist_id={}".format(playlist_id)
+        + "&track_ids={}".format(track_ids)
+        + "&user_auth_token={}".format(user_auth_token)
+        + "&app_id={}".format(qobuz.api.APP_ID)
+    )
+
+
+def test_playlist_add_tracks(playlist, user):
+    track_ids = ",".join(
+        [str(t["id"]) for t in playlist_get_tracks_json["tracks"]["items"]]
+    )
+
+    with responses.RequestsMock() as response_mock:
+        response_mock.add(
+            responses.GET,
+            get_url(
+                playlist_id=playlist_add_tracks_json["id"],
+                user_auth_token=user.auth_token,
+                track_ids=track_ids,
+            ),
+            json=playlist_add_tracks_json,
+            status=200,
+            match_querystring=True,
+        )
+
+        tracks = [
+            qobuz.Track(t) for t in playlist_get_tracks_json["tracks"]["items"]
+        ]
+
+        # Match playlist-ids to add to the correct id
+        playlist.id = playlist_add_tracks_json["id"]
+
+        playlist.add_tracks(tracks, user)
