3bf4555cd6b0e65a536024242b20e6f1482c60a1
taschenb <taschenb@posteo.de>
2019-04-09 10:04:11 +0200

    test: response: Add favorite/getUserFavorites
    Add the example response from the API-documentation for requesting an
    user's favorites.
    
    Request:
    http://www.qobuz.com/api.json/0.2/
    favorite/getUserFavorites
    ?app_id=100000000
    &limit=2
    &type=albums
    &user_auth_token=MyAuThTokEn

--- tests/resources/responses/__init__.py
+++ tests/resources/responses/__init__.py
@@ -15,5 +15,8 @@ with open("tests/resources/responses/artist_search.json") as json_data:
 with open("tests/resources/responses/user_login.json") as json_data:
     user_login_json = json.load(json_data)
 
+with open("tests/resources/responses/user_fav_get_albums.json") as json_data:
+    user_fav_get_albums_json = json.load(json_data)
+
 with open("tests/resources/responses/track_search.json") as json_data:
     track_search_json = json.load(json_data)
