6a8bad0f1d787d1d825e9c7847c820dca55b172b
taschenb <taschenb@posteo.de>
2019-04-09 10:08:09 +0200

    test: user: Test getting favorite albums
    Test for getting an user's favorite albums, as the API-documentation
    has a example response to that

--- tests/user/test_user_favorites.py
+++ tests/user/test_user_favorites.py
@@ -3,9 +3,22 @@ import qobuz
 import responses
 
 from tests.resources.fixtures import user, album
+from tests.resources.responses import user_fav_get_albums_json
 
 
 qobuz.api.APP_ID = "request_from_api@qobuz.com"
+
+
+def get_favorite_albums_url(user_auth_token, limit=50, offset=0):
+    return (
+        qobuz.api.API_URL
+        + "favorite/getUserFavorites"
+        + "?type=album"
+        + "&limit={}".format(limit)
+        + "&offset={}".format(offset)
+        + "&user_auth_token={}".format(user_auth_token)
+        + "&app_id={}".format(qobuz.api.APP_ID)
+    )
 
 
 def get_favorite_add_albums_url(album_ids, user_auth_token):
@@ -26,6 +39,43 @@ def get_favorite_del_albums_url(album_ids, user_auth_token):
         + "&user_auth_token={}".format(user_auth_token)
         + "&app_id={}".format(qobuz.api.APP_ID)
     )
+
+
+@pytest.fixture
+def response_fav_get_albums(user):
+    with responses.RequestsMock() as response_mock:
+        response_mock.add(
+            responses.GET,
+            url=get_favorite_albums_url(user.auth_token),
+            json=user_fav_get_albums_json,
+            status=200,
+            match_querystring=True,
+        )
+
+        yield response_mock
+
+
+def test_user_favorite_get_albums_type(user, response_fav_get_albums):
+    albums = user.favorites_get(fav_type="album")
+
+    for a in albums:
+        assert isinstance(a, qobuz.Album)
+
+
+def test_user_favorite_get_albums_len(user, response_fav_get_albums):
+    albums = user.favorites_get(fav_type="album")
+
+    assert len(albums) == user_fav_get_albums_json["albums"]["limit"]
+
+
+def test_user_favorite_get_albums_content(user, response_fav_get_albums):
+    albums = user.favorites_get(fav_type="album")
+
+    for i in range(len(albums)):
+        assert albums[i] == qobuz.Album(
+            user_fav_get_albums_json["albums"]["items"][i]
+        )
+
 
 def test_user_favorite_add_albums(user, album):
     fav_add_album_url = get_favorite_add_albums_url(album.id, user.auth_token)
