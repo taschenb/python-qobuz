68383e9b0f5cdd8ea7171de614a3da05dc76bc7c
taschenb <taschenb@posteo.de>
2019-05-01 05:10:07 +0200

    feat: api: Sign calls with the secret when requested
    Some API-calls need the secret for the app_id, namely
    track/getFileUrl.
    
    The secret is thereby not transferred directly but rather used in
    generating a checksum over every request.

--- src/qobuz/api.py
+++ src/qobuz/api.py
@@ -1,5 +1,7 @@
+import hashlib
 import qobuz
 import requests
+import time
 try:
     from urllib.parse import urljoin
 except ImportError:
@@ -26,12 +28,12 @@ def register_app(app_id, app_secret=None):
     qobuz.api.APP_SECRET = app_secret
 
 
-def request(url, comma_encoding=True, **params):
+def request(url, signed=False, comma_encoding=True, **params):
     """Call Qobuz API for URL and parameters.
 
-    Call the API_URL, append the url and parameters.
-    In order to be accepted, the call needs a valid APP_ID.
-    An APP_ID can be requested from api@qobuz.com.
+    In order to be accepted, every call needs a valid APP_ID.
+    Some calls also require the corresponding secret to be added as signature.
+    Both the ID and secret can be requested from api@qobuz.com.
 
     >>> import qobuz
     >>> qobuz.api.register_app(app_id="your_app_id", app_secret="your secret")
@@ -40,11 +42,19 @@ def request(url, comma_encoding=True, **params):
     ----------
     url: str
         URL to be joined with the base URL API_URL
+    signed: bool
+        Sign the request with the APP secret
     comma_encoding: bool
         URL encode ',' as '%2C'
     **kwargs
         GET parameters to be added to the request
     """
+    if signed is True:
+        request_ts = int(time.time())
+        params["request_sig"] = _get_request_sig(
+            urljoin(API_URL, url), timestamp=request_ts, **params
+        )
+        params["request_ts"] = request_ts
     if comma_encoding is True:
         return _request(url, **params)
     else:
@@ -92,3 +102,49 @@ def _request_comma_params(url, **params):
     url = "{}?{}".format(urljoin(API_URL, url), params)
 
     return _request(url)
+
+
+def _get_request_sig(url, timestamp, **params):
+    """Return the signature for a request.
+
+    Whenever an API-call requires the app-secret, the secret is not sent
+    directly, but is hidden inside a checksum for that request, called
+    signature.
+
+    This signature needs to be added as 'request_sig' to the GET parameters.
+
+    Parameters
+    ----------
+    url: str
+        Url for the API call
+    timestamp: int
+        UNIX timestamp in seconds
+    **kwargs
+        GET parameters of the request
+
+    Returns
+    -------
+    str
+        request_sig to be added to the GET parameters
+    """
+    if qobuz.api.APP_SECRET is None:
+        raise TypeError("No secret setup. See api.register_app()")
+
+    request_sig = url.replace(API_URL, "")
+    request_sig = request_sig.replace("/", "")
+
+    # The signature does not include all parameters
+    params.pop("app_id", None)
+    params.pop("user_auth_token", None)
+    params.pop("request_ts", None)
+
+    params = dict(sorted(params.items()))
+
+    for key, value in params.items():
+        if value is not None:
+            request_sig += str(key) + str(value)
+
+    request_sig += str(timestamp)
+    request_sig += APP_SECRET
+
+    return hashlib.md5(request_sig.encode()).hexdigest()
